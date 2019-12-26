41db9720c86321d78f3886febce35cc85380814c
Paul Stone <pmstone247@gmail.com>
2019-12-26 00:53:21 -0700

    set up table with keypress listeners

---
+++ src/app/FlexTableComponent/flextable.component.ts
@@ -0,0 +1,117 @@
+import { Component, Input, AfterViewInit } from '@angular/core';
+const arrowKeys = [
+  'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight' 
+]
+
+@Component({
+  selector: 'flex-table',
+  templateUrl: './flextable.component.html',
+  styleUrls: ['./flextable.component.scss']
+})
+
+export class FlexTableComponent implements AfterViewInit {
+  @Input() columns: any[]
+  @Input() data: any[]
+  @Input() width: string
+  @Input() height: string
+
+  selectedCellId: string
+  currSelectedCell: any
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.setClickEvents()
+    this.setKeyPressListeners()
+  }
+
+  setClickEvents() {
+    this.columns.forEach((col, index) => {
+      document.getElementById(`table-header-${index}`).addEventListener("click", (event) => {
+        console.log('clicking the header!', event)
+      })
+    })
+
+    this.data.forEach((row, rowIndex) => {
+      this.columns.forEach((col, index) => {
+        document.getElementById(`table-cell-${rowIndex}-${index}`).addEventListener("click", (event) => {
+          const currSelectedCell = document.getElementById(this.selectedCellId)
+          const selectedCellId = event.target['id']
+
+          if (currSelectedCell && this.selectedCellId !== selectedCellId) {
+            currSelectedCell.className = ''
+          }
+
+          this.selectedCellId = selectedCellId
+          this.currSelectedCell = document.getElementById(this.selectedCellId)
+          event.target['className'] = 'highlighted'
+        })
+
+      })
+    })
+
+  }
+
+  setKeyPressListeners() {
+    document.addEventListener("keydown", (event) => {
+      if (arrowKeys.includes(event.code) && this.currSelectedCell) {
+        const rowIndex = Number(this.currSelectedCell['dataset']['rowId'])
+        const colIndex = Number(this.currSelectedCell['dataset']['columnId'])
+        let nextCellId
+
+        if (event.code === 'ArrowUp') {
+          // there is a row above the current cell selected
+          // so we can navigate up to the cell above
+          if (rowIndex > 0) {
+            nextCellId = `table-cell-${rowIndex - 1}-${colIndex}`
+          } else {
+            if (colIndex > 0) {
+              nextCellId = `table-cell-${this.data.length - 1}-${colIndex - 1}`
+            } else {
+              nextCellId = `table-cell-${this.data.length - 1}-${this.columns.length - 1}`
+            }
+          }
+        } else if (event.code === 'ArrowDown') {
+          // if current row is not the bottom row
+          if (rowIndex < (this.data.length - 1)) {
+            nextCellId = `table-cell-${rowIndex + 1}-${colIndex}`
+          } else {
+            if (colIndex < (this.columns.length - 1)) {
+              // we can navigate to the right in the first row
+              nextCellId = `table-cell-0-${colIndex + 1}`
+            } else {
+              nextCellId = 'table-cell-0-0'
+            }
+          }
+        } else if (event.code === 'ArrowLeft') {
+          if (colIndex > 0) {
+            nextCellId = `table-cell-${rowIndex}-${colIndex - 1}`
+          } else {
+            if (rowIndex > 0) {
+              nextCellId = `table-cell-${rowIndex - 1}-${this.columns.length - 1}`
+            } else {
+              nextCellId = `table-cell-${this.data.length - 1}-${this.columns.length - 1}`
+            }
+          }
+        } else if (event.code === 'ArrowRight') {
+          if (colIndex < (this.columns.length - 1)) {
+            nextCellId = `table-cell-${rowIndex}-${colIndex + 1}`
+          } else {
+            if (rowIndex < (this.data.length - 1)) {
+              nextCellId = `table-cell-${rowIndex + 1}-0`
+            } else {
+              nextCellId = 'table-cell-0-0'
+            }
+          }
+        }
+
+        const nextCell = document.getElementById(nextCellId)
+        if (nextCell) {
+          this.currSelectedCell.className = ''
+          nextCell.className = 'highlighted'
+          this.currSelectedCell = nextCell
+        }
+      }
+    })
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'flex-table';
+  columns = [
+    {
+      title: 'column title 1',
+      type: 'input',
+      class: '',
+      data: 'name'
+    },
+    {
+      title: 'column title 2',
+      type: 'input',
+      class: '',
+      data: 'code'
+    },
+    {
+      title: 'column title 3',
+      type: 'input',
+      class: '',
+      data: 'description'
+    }
+  ]
+
+  data = [
+    {
+      name: 'Alice Cooper',
+      code: '12345',
+      description: 'TEST'
+    },
+    {
+      name: 'Bob Thunder',
+      code: '98765',
+      description: 'TEST'
+    },
+    {
+      name: 'Jay Swift',
+      code: '09578',
+      description: 'TEST'
+    }
+  ]
+}
+
+export interface Column {
+  type: String,
+  class: String,
+  title: String
 }
