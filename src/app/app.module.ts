fa888e51b94575fdba55c6117547387eb0848d0f
Paul Stone <pmstone247@gmail.com>
2019-12-29 11:50:03 -0700

    adding dropdown import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FlexTableComponent } from './FlexTableComponent/flextable.component';
 import { InputComponent } from './FieldTypes/InputComponent/input.component';
 import { CheckboxComponent } from './FieldTypes/CheckboxComponent/checkbox.component';
+import { DropdownComponent } from './FieldTypes/DropdownComponent/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FlexTableComponent,
     InputComponent,
-    CheckboxComponent
+    CheckboxComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule
